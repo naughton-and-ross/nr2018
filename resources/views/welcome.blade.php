86c44e04b1b3bd4dafc49add73d09e61c97a3047
William Naughton-Gravette <william@willng.me>
2017-12-22 00:51:55 +1100

    UI progress

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,30 +5,27 @@
         <title>Commodity Creative</title>
         <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css" integrity="sha384-nn4HPE8lTHyVtfCBi5yW9d20FjT8BJwUXyWZT9InLYax14RDjBj46LmSztkmNP9w" crossorigin="anonymous">
         <link rel="stylesheet" href="{{asset('css/app.css')}}" />
+        <!--[if lte IE 8]>
+            <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/grids-responsive-old-ie-min.css">
+        <![endif]-->
+        <!--[if gt IE 8]><!-->
+            <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/grids-responsive-min.css">
+        <!--<![endif]-->
     </head>
     <body>
         <section class="home-hero">
             <div class="pure-g">
-                <div class="pure-u-6-24">
+                <div class="pure-u-1 pure-u-xl-7-24">
 
                 </div>
-                <div class="pure-u-1-2">
-
-                </div>
-                <div class="pure-u-6-24">
-
-                </div>
-                <div class="pure-u-6-24">
-
-                </div>
-                <div class="pure-u-10-24 hero-text">
+                <div class="pure-u-1 pure-u-xl-10-24 hero-text">
                     <div class="l-box">
                         <h1>Commodity Creative is a design & development studio based in Melbourne, Australia.</h1>
                         <h1>Focusing our work on Web Design & Development, we can also help with graphic design, print and publishing design, stationery, and photography.</h1>
-                        <h1>You're looking at some of our work. Here's <b>Little Sports Heroes</b>.</h1>
+                        <h1>You're looking at some of our work. Here's <b>Project Title</b>.</h1>
                     </div>
                 </div>
-                <div class="pure-u-6-24">
+                <div class="pure-u-1 pure-u-xl-7-24">
 
                 </div>
             </div>
@@ -37,23 +34,23 @@
             <div class="pure-g">
                 <div class="pure-u-1">
                     <div class="l-box">
-                        <h1></h1>
+                        <h1>Spotlight: Some Recent Highlights</h1>
                     </div>
                 </div>
             </div>
         </section>
         <!-- section class="tiles">
             <div class="pure-g">
-                <div class="pure-u-1 intro">
+                <div class="pure-u-xl-1 intro">
                     <h1>Just a taste of what we do</h1>
                 </div>
-                <div class="pure-u-2-24">
+                <div class="pure-u-xl-2-24">
 
                 </div>
-                <div class="pure-u-9-24">
+                <div class="pure-u-xl-9-24">
                     <div class="l-box" style="background-color: white;">
                         <img src="{{asset('img/tom4.jpg')}}" class="pure-img" />
-                        <div class="pure-u-16-24">
+                        <div class="pure-u-xl-16-24">
                             <h2>Project Title</h2>
                             <p>
                                 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
@@ -61,10 +58,10 @@
                         </div>
                     </div>
                 </div>
-                <div class="pure-u-2-24">
+                <div class="pure-u-xl-2-24">
 
                 </div>
-                <div class="pure-u-9-24">
+                <div class="pure-u-xl-9-24">
                     <div class="l-box" style="background-color: white;">
                         <img src="{{asset('img/tom3.jpg')}}" class="pure-img" />
                         <h2>Project Title</h2>
@@ -73,13 +70,13 @@
                         </p>
                     </div>
                 </div>
-                <div class="pure-u-2-24">
+                <div class="pure-u-xl-2-24">
 
                 </div>
-                <div class="pure-u-2-24">
+                <div class="pure-u-xl-2-24">
 
                 </div>
-                <div class="pure-u-9-24">
+                <div class="pure-u-xl-9-24">
                     <div class="l-box" style="background-color: white;">
                         <img src="{{asset('img/tom1.jpg')}}" class="pure-img" />
                         <h2>Project Title</h2>
@@ -92,17 +89,20 @@
         </section-->
         <section class="spotlight">
             <div class="pure-g spotlight-tile black">
-                <div class="pure-u-1-24"></div>
-                <div class="pure-u-2-24 text">
+                <div class="pure-u-1 pure-u-xl-1-24"></div>
+                <div class="pure-u-1 pure-u-xl-2-24 text">
                     <div class="l-box">
                         <h1>Resist the Dutton Home Office</h1>
+                        <p>
+                            See more &rarr;
+                        </p>
                     </div>
                 </div>
-                <div class="pure-u-16-24 image">
+                <div class="pure-u-1 pure-u-xl-16-24 image">
                     <img class="pure-img" src="{{asset('img/tom4.jpg')}}" />
                 </div>
-                <div class="pure-u-1-24"></div>
-                <div class="pure-u-3-24 text">
+                <div class="pure-u-1 pure-u-xl-1-24"></div>
+                <div class="pure-u-1 pure-u-xl-3-24 text">
                     <div class="l-box">
                         <p>
                             The Australian Government has been incrementally expanding its powers to retain identiying informations from Australians and establish a permanent state of exception. They have recently announced their plan to use images from state drivers licenses and map these with other personal biometric information to create a national database of faces.
@@ -114,17 +114,8 @@
                 </div>
             </div>
             <div class="pure-g spotlight-tile blue">
-                <div class="pure-u-1-24"></div>
-                <div class="pure-u-2-24 text">
-                    <div class="l-box">
-                        <h1>Resist the Dutton Home Office</h1>
-                    </div>
-                </div>
-                <div class="pure-u-16-24 image">
-                    <img class="pure-img" src="{{asset('img/tom4.jpg')}}" />
-                </div>
-                <div class="pure-u-1-24"></div>
-                <div class="pure-u-3-24 text">
+                <div class="pure-u-1 pure-u-xl-1-24"></div>
+                <div class="pure-u-1 pure-u-xl-3-24 text">
                     <div class="l-box">
                         <p>
                             The Australian Government has been incrementally expanding its powers to retain identiying informations from Australians and establish a permanent state of exception. They have recently announced their plan to use images from state drivers licenses and map these with other personal biometric information to create a national database of faces.
@@ -134,6 +125,19 @@
                         </p>
                     </div>
                 </div>
+                <div class="pure-u-1 pure-u-xl-16-24 image">
+                    <img class="pure-img" src="{{asset('img/tom2.jpg')}}" />
+                </div>
+                <div class="pure-u-1 pure-u-xl-2-24 text right">
+                    <div class="l-box">
+                        <h1>Virtual Nation: Sovereignty Without Borders</h1>
+                        <p>
+                            See more &rarr;
+                        </p>
+                    </div>
+                </div>
+                <div class="pure-u-1 pure-u-xl-1-24"></div>
+
             </div>
         </section>
     </body>
