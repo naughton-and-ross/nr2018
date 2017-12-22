7091e37069fee8a90bebeb39c84515509a3d4f17
William Naughton-Gravette <william@willng.me>
2017-12-22 23:30:48 +1100

    UI gross progress

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -41,6 +41,69 @@
                 </div>
             </div>
         </section>
+        <section class="info">
+            <div class="pure-g">
+                <div class="pure-u-1 pure-u-xl-2-24"></div>
+                <div class="pure-u-5-24 info-tile">
+                    <div class="l-box">
+                        <h1>&#8212;</h1>
+                        <h1>Web Development</h1>
+                        <h2>Beautiful, snappy websites. Always mobile-ready.</h2>
+                    </div>
+                </div>
+                <div class="pure-u-5-24 info-tile">
+                    <div class="l-box">
+                        <h1>&#8212;</h1>
+                        <h1>Brand Development</h1>
+                        <h2>Your business relies on a perfect brand. We can give it to you.</h2>
+                    </div>
+                </div>
+                <div class="pure-u-5-24 info-tile">
+                    <div class="l-box">
+                        <h1>&#8212;</h1>
+                        <h1>Advertising</h1>
+                        <h2>A killer campaign can drive business for years, but it needs to be done right.</h2>
+                    </div>
+                </div>
+                <div class="pure-u-5-24 info-tile">
+                    <div class="l-box">
+                        <h1>&#8212;</h1>
+                        <h1>Social &amp; SEO</h1>
+                        <h2>A perfect web presence is nothing if nobody can find you. Our SEO services can help with that.</h2>
+                    </div>
+                </div>
+                <div class="pure-u-1 pure-u-xl-2-24"></div>
+                <div class="pure-u-1 pure-u-xl-2-24"></div>
+                <div class="pure-u-5-24 info-tile">
+                    <div class="l-box">
+                        <h1>&#8212;</h1>
+                        <h1>Print Design</h1>
+                        <h2>Looking to publish? We can build a manuscript, and help with self-publishing.</h2>
+                    </div>
+                </div>
+                <div class="pure-u-5-24 info-tile">
+                    <div class="l-box">
+                        <h1>&#8212;</h1>
+                        <h1>Photography</h1>
+                        <h2>Bring us in to photograph your team or office.</h2>
+                    </div>
+                </div>
+                <div class="pure-u-5-24 info-tile secondary">
+                    <div class="l-box">
+                        <h1>&#8212;</h1>
+                        <h1>Brand Consultation</h1>
+                        <h2>Sometimes all you need is sage advice. We're here for that too.</h2>
+                    </div>
+                </div>
+                <div class="pure-u-5-24 info-tile secondary">
+                    <div class="l-box">
+                        <h1>&#8212;</h1>
+                        <h1>Copywriting</h1>
+                    </div>
+                </div>
+                <div class="pure-u-1 pure-u-xl-2-24"></div>
+            </div>
+        </section>
         <section class="home-text">
             <div class="pure-g">
                 <div class="pure-u-1">
@@ -155,7 +218,7 @@
 
                 <div class="pure-u-1 pure-u-xl-2-24 text">
                     <div class="l-box">
-                        <h1>Virtual Nation: Sovereignty Without Borders</h1>
+                        <h1>Natus Nut Mix</h1>
                         <p>
                             See more &rarr;
                         </p>
@@ -169,29 +232,12 @@
                 <div class="pure-u-1 pure-u-xl-3-24 text">
                     <div class="l-box bottom">
                         <p>
-                            The Australian Government has been incrementally expanding its powers to retain identiying informations from Australians and establish a permanent state of exception. They have recently announced their plan to use images from state drivers licenses and map these with other personal biometric information to create a national database of faces.
-                        </p>
-                        <p>
-                            The plans are vague and do not guarantee security of Information. Let your voice be heard and contact your Federal MP and request more information about data retention and privacy of your biometric data.
+                            Care for a nut?
                         </p>
                     </div>
                 </div>
             </div>
             <div class="pure-g spotlight-tile blue">
-                <div class="pure-u-1 pure-u-xl-1-24"></div>
-
-                <div class="pure-u-1 pure-u-xl-2-24 text">
-                    <div class="l-box">
-                        <h1>Virtual Nation: Sovereignty Without Borders</h1>
-                        <p>
-                            See more &rarr;
-                        </p>
-                    </div>
-                </div>
-
-                <div class="pure-u-1 pure-u-xl-16-24 image">
-                    <img class="pure-img" src="https://farm1.staticflickr.com/430/18143519083_85c8c66813_k_d.jpg" />
-                </div>
                 <div class="pure-u-1 pure-u-xl-1-24"></div>
                 <div class="pure-u-1 pure-u-xl-3-24 text">
                     <div class="l-box bottom">
@@ -203,6 +249,19 @@
                         </p>
                     </div>
                 </div>
+                <div class="pure-u-1 pure-u-xl-16-24 image">
+                    <img class="pure-img" src="https://farm1.staticflickr.com/430/18143519083_85c8c66813_k_d.jpg" />
+                </div>
+                <div class="pure-u-1 pure-u-xl-2-24 text right">
+                    <div class="l-box">
+                        <h1>Four Pillars Gin Distillery</h1>
+                        <p>
+                            See more &rarr;
+                        </p>
+                    </div>
+                </div>
+                <div class="pure-u-1 pure-u-xl-1-24"></div>
+
             </div>
         </section>
     </body>
